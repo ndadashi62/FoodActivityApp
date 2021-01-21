435eeaa97c79f8e01ce50c8c7f7b53838b811145
ndadashi62 <nasim.dadashi@gmail.com>
2021-01-21 11:18:11 -0500

    value of edit text costumername passes to the order activity page

--- app/src/main/java/com/example/foodactivityapp/MainActivity.kt
+++ app/src/main/java/com/example/foodactivityapp/MainActivity.kt
@@ -11,14 +11,19 @@ import kotlinx.android.synthetic.main.activity_order.*
 
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        radioWilensky.setOnClickListener {
 
-        }
+
+
+
+
 
         btnOrder.setOnClickListener {
             order()
@@ -30,9 +35,27 @@ class MainActivity : AppCompatActivity() {
 
 
         if (radioWilensky.isChecked == true) {
-
-
             val intent = Intent(this@MainActivity, OrderActivity::class.java)
+
+            var tvDisplaCostumerName: String = editTextCostumerName.text.toString()
+
+            var wilensky: String = radioWilensky.text.toString()
+
+          //  var poutine: String = radioWilensky.text.toString()
+            //var pizza: String = radioWilensky.text.toString()
+           // var burger: String = radioWilensky.text.toString()
+           // var greenSalad: String = radioWilensky.text.toString()
+           // var bagales: String = radioWilensky.text.toString()
+            
+            intent.putExtra("costumername", tvDisplaCostumerName)
+
+            intent.putExtra("wilsky", wilensky)
+           // intent.putExtra("poutine", poutine)
+            //intent.putExtra("pizza", pizza)
+           // intent.putExtra("burger", burger)
+           // intent.putExtra("greenSalad", greenSalad)
+           // intent.putExtra("bageles", bagales)
+
             startActivity(intent)
 
 
--- app/src/main/java/com/example/foodactivityapp/OrderActivity.kt
+++ app/src/main/java/com/example/foodactivityapp/OrderActivity.kt
@@ -2,17 +2,35 @@ package com.example.foodactivityapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_order.*
 
+
 class OrderActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_order)
 
 
-        var tvDisplayordername = textViewDisplayOrderName.setText("wilesky").toString()
+        var displaycustumername = intent.getStringExtra("costumername")
+        var tvdisplaycostumername = textViewCustomerName.setText(displaycustumername)
+
+        var wilsky = intent.getStringExtra("wilsky")
+        var tvWilsky = textViewDisplayOrderName.setText(wilsky)
+
+//        var poutine = intent.getStringExtra("pizza")
+//        var tvpoutine = textViewDisplayOrderName.setText(poutine)
+//
+//        var pizza = intent.getStringExtra("pizza")
+//        var tvpizza = textViewDisplayOrderName.setText(pizza)
+//
+//        var burger = intent.getStringExtra("burger")
+//        var tvBurger = textViewDisplayOrderName.setText(burger)
 
 
 
