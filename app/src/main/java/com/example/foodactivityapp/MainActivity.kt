d10a5dd5775fe4a05a865e5f701ede87f31f97c6
ndadashi62 <nasim.dadashi@gmail.com>
2021-01-20 19:30:25 -0500

    order button functioning with the first radio button

--- app/src/main/java/com/example/foodactivityapp/MainActivity.kt
+++ app/src/main/java/com/example/foodactivityapp/MainActivity.kt
@@ -1,13 +1,62 @@
 package com.example.foodactivityapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.CompoundButton
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_order.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        radioWilensky.setOnClickListener {
+
+        }
+
+        btnOrder.setOnClickListener {
+            order()
+        }
+
+    }
+
+    fun order() {
+
+
+        if (radioWilensky.isChecked == true) {
+
+
+            val intent = Intent(this@MainActivity, OrderActivity::class.java)
+            startActivity(intent)
+
+
+        }
+
+
     }
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/example/foodactivityapp/OrderActivity.kt
+++ app/src/main/java/com/example/foodactivityapp/OrderActivity.kt
@@ -2,10 +2,20 @@ package com.example.foodactivityapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_order.*
 
 class OrderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_order)
+
+
+        var tvDisplayordername = textViewDisplayOrderName.setText("wilesky").toString()
+
+
+
+
     }
 }
